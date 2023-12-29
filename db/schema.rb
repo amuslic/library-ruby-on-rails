2e74edd00b813ed0267df2f940142fce2c20c733
Aneta Muslić <Aneta.Muslic@blu.com>
2023-12-29 15:11:12 +0100

    Added role column to the user table
    for authentication, added user roles in table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_29_135241) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_29_141002) do
   create_table "authors", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_29_135241) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "role"
   end
 
   add_foreign_key "books", "authors"
